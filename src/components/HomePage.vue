3cb8cbef19dc22c922cac6b17baa8eda178afba9
Hubbs <1woue@vcs2go.net>
2023-04-11 14:50:42 -0400

    Home Page Testing

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,25 +2,11 @@
   <h1>2152 SMASH Scouting</h1>
   <img :src="absoluteLogoPath" alt="Cannot load logo file" class="center" width="300" height="75">
   <h2>Scouting Forms</h2>
-  <ul v-if="list.length > 0" class="link-list">
-    <li v-for="[i, name] of list.entries()" :key="i">
-      <RouterLink :to="{ name: 'form', query: { name } }">{{ name }}</RouterLink>
-    </li>
-  </ul>
-  <p v-else>No configurations specified.</p>
-  <h2>Tools</h2>
-  <ul class="link-list">
-    <li>
-      <RouterLink :to="{ name: 'inspector' }">Data Inspector</RouterLink>
-    </li>
-    <li>
-      <a href="https://docs.google.com/document/d/1-HgbNw-TdRhQ4DJKG9C3BOcHSc-WlC92WXWyaqcHNgc/edit?usp=sharing">Scouting Guide</a>
-    </li>
-  </ul>
+  
   <br>
   <br>
   <footer>
-    <h5>Version 1.3.0</h5>
+    <h5>Version 1.0.0</h5>
   </footer>
 </template>
 
